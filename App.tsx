b9f0b2c1bb7196133279f856cdf363360a7a1908
Dorian Lorek <36369324+EloGamerr@users.noreply.github.com>
2025-03-21 17:18:35 +0100

    Add App.tsx main component

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            title: 'Welcome'
+          }}
+        />
+        <Stack.Screen 
+          name="Details" 
+          component={DetailsScreen} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
